c32ead9356daad5db2166c50eca7a0f3f69f00ad
Do Tran <do.tran@saigontechnology.com>
2023-04-27 23:17:26 +0700

    add a bit code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import InputBox from './inputbox/inputbox';
 
 function App() {
+  const [focusId, setFocus] = useState(1);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App h-full">
+      <div className='flex grow flex-row flex-wrap h-full bg-slate-200'>
+        <div className="basis-1/3 flex grow">
+          <InputBox content={'hello'} id={1}></InputBox>
+        </div>
+        <div className="basis-1/3 flex grow">
+          <InputBox content={'hello 2'} id={2}></InputBox>
+        </div>
+        <div className="basis-1/3 flex grow">
+          <InputBox content={'hello 3'} id={3}></InputBox>
+        </div>
+      </div>
     </div>
   );
 }
